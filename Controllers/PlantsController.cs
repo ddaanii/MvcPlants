11a987131759aeaf754a8475bc533478eb89a827
ddaanii <torxdani1@gmail.com>
2024-05-15 11:47:23 +0200

    creazione controller, modello e prima vista (lavoro di gruppo in classe)

---
+++ Controllers/PlantsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcPlants.Controllers
+{
+    public class PlantsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Plant.cs
@@ -0,0 +1,13 @@
+﻿namespace MvcPlants.Models
+{
+    public class Plant
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? ImgPath { get; set; }
+        public string? Family { get; set; }
+        public string? Genus { get; set; }
+        public string? Species { get; set; }
+        public string? Origin { get; set; }
+    }
+}
