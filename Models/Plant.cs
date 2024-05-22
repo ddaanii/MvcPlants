91c5155d94cd0c0dee957fdac9591c15d3174ff9
ddaanii <torxdani1@gmail.com>
2024-05-22 11:49:43 +0200

    configurato DRY

--- Models/Plant.cs
+++ Models/Plant.cs
@@ -1,13 +1,37 @@
-﻿namespace MvcPlants.Models
+﻿using System.ComponentModel.DataAnnotations;
+using static System.Net.WebRequestMethods;
+using System.Runtime.InteropServices;
+
+namespace MvcPlants.Models
 {
     public class Plant
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [RegularExpression(@"^[a-zA-Z\s]+$")]
+        [Required]
         public string? Name { get; set; }
+
+        [RegularExpression(@"^\b(https ?:\/\/)?[\w -] + (\.[\w -] +)+\b$")]
+        [Required]
         public string? ImgPath { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [RegularExpression(@"^[a-zA-Z\s]+$")]
         public string? Family { get; set; }
+        
+        [StringLength(60, MinimumLength = 3)]
+        [RegularExpression(@"^[a-zA-Z\s]+$")]
         public string? Genus { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [RegularExpression(@"^[a-zA-Z\s]+$")]
         public string? Species { get; set; }
+
+        [MinLength(3)]
+        [RegularExpression(@"^[a-zA-Z.\s]+$")]
+        [Required]
         public string? Origin { get; set; }
     }
 }
