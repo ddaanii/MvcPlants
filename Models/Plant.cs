a49f9bf2b3438ac6e421a13590bd4f32adf4239e
ddaanii <torxdani1@gmail.com>
2024-05-25 22:44:20 +0200

    Modificati messaggi di errore per regex (lavoro svolto da Giulia che non ha potuto fare il commit per dei problemi come VS)

--- Models/Plant.cs
+++ Models/Plant.cs
@@ -4,33 +4,33 @@ using System.Runtime.InteropServices;
 
 namespace MvcPlants.Models
 {
-    public class Plant
-    {
-        public int Id { get; set; }
+	public class Plant
+	{
+		public int Id { get; set; }
 
-        [StringLength(60, MinimumLength = 3)]
-        [RegularExpression(@"^[a-zA-Z\s]+$")]
-        [Required]
-        public string? Name { get; set; }
+		[StringLength(60, MinimumLength = 3)]
+		[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Warning: The Name is not valid. Please ensure you use only letters (uppercase or lowercase), spaces, and periods.")]
+		[Required]
+		public string? Name { get; set; }
 
-        [Required]
-        public string? ImgPath { get; set; }
+		[Required]
+		public string? ImgPath { get; set; }
 
-        [StringLength(60, MinimumLength = 3)]
-        [RegularExpression(@"^[a-zA-Z\s]+$")]
-        public string? Family { get; set; }
-        
-        [StringLength(60, MinimumLength = 3)]
-        [RegularExpression(@"^[a-zA-Z\s]+$")]
-        public string? Genus { get; set; }
+		[StringLength(60, MinimumLength = 3)]
+		[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Warning: The Species is not valid. Please ensure you use only letters (uppercase or lowercase) and spaces")]
+		public string? Family { get; set; }
 
-        [StringLength(60, MinimumLength = 3)]
-        [RegularExpression(@"^[a-zA-Z\s]+$")]
-        public string? Species { get; set; }
+		[StringLength(60, MinimumLength = 3)]
+		[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Warning: The Species is not valid. Please ensure you use only letters (uppercase or lowercase) and spaces")]
+		public string? Genus { get; set; }
 
-        [MinLength(3)]
-        [RegularExpression(@"^[a-zA-Z.\s]+$")]
-        [Required]
-        public string? Origin { get; set; }
-    }
+		[StringLength(60, MinimumLength = 3)]
+		[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Warning: The Species is not valid. Please ensure you use only letters (uppercase or lowercase) and spaces")]
+		public string? Species { get; set; }
+
+		[MinLength(3)]
+		[RegularExpression(@"^[a-zA-Z.\s]+$", ErrorMessage = "Warning: The Origin is not valid. Please ensure you use only letters (uppercase or lowercase), spaces, and periods.")]
+		[Required]
+		public string? Origin { get; set; }
+	}
 }
